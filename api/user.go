392e8747daa553df131a40ac88d60aa6ea12dfac
eron.runner@gmail.com <eron.runner@gmail.com>
2020-03-19 11:47:53 +0700

    Task 4: create User profile endpoint

--- api/user.go
+++ api/user.go
@@ -12,21 +12,45 @@ import (
 // AddUserRoutes - Add routes of USER to sub router
 func AddUserRoutes(sub *mux.Router) {
 	sub.Handle("/login", util.AppHandler(login)).Methods("POST")
-	sub.Handle("/registration", util.AppHandler(register)).Methods("POST")
-	sub.Handle("/profile-image", util.AppHandler(uploadProfileImage)).Methods("POST")
+	sub.Handle("/registration", util.AppHandler(registerUser)).Methods("POST")
+	sub.Handle("/profile-image", util.AppHandler(uploadUserProfileImage)).Methods("POST")
+	sub.Handle("/{id}", util.AppHandler(updateUserInfo)).Methods("PUT")
+	sub.Handle("/{id}/pass", util.AppHandler(changeUserPassword)).Methods("PUT")
+	sub.Handle("/{id}/notifications", util.AppHandler(getUserNotifications)).Methods("GET")
+	sub.Handle("/{id}/perks", util.AppHandler(getUserPerks)).Methods("GET")
 }
 
 func login(w http.ResponseWriter, r *http.Request) (interface{}, *error.AppError) {
-	
+
 	return "", nil
 }
 
-func register(w http.ResponseWriter, r *http.Request) (interface{}, *error.AppError) {
-	
+func registerUser(w http.ResponseWriter, r *http.Request) (interface{}, *error.AppError) {
+
 	return "", nil
 }
 
-func uploadProfileImage(w http.ResponseWriter, r *http.Request) (interface{}, *error.AppError) {
-	
+func uploadUserProfileImage(w http.ResponseWriter, r *http.Request) (interface{}, *error.AppError) {
+
+	return "", nil
+}
+
+func updateUserInfo(w http.ResponseWriter, r *http.Request) (interface{}, *error.AppError) {
+
+	return "", nil
+}
+
+func changeUserPassword(w http.ResponseWriter, r *http.Request) (interface{}, *error.AppError) {
+
+	return "", nil
+}
+
+func getUserNotifications(w http.ResponseWriter, r *http.Request) (interface{}, *error.AppError) {
+
+	return "", nil
+}
+
+func getUserPerks(w http.ResponseWriter, r *http.Request) (interface{}, *error.AppError) {
+
 	return "", nil
 }
