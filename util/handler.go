a9e711846c645cb43b768320f7b2fa6bda8f8242
eron.runner@gmail.com <eron.runner@gmail.com>
2020-03-22 20:58:28 +0700

    Add MiddlewareChain

--- util/handler.go
+++ util/handler.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"context"
 	"encoding/json"
 	"log"
 	"net/http"
@@ -8,8 +9,31 @@ import (
 	"github.com/greennit/error"
 )
 
+
+type MiddlewareChain struct {
+	MiddlewareHandlers []*func (w http.ResponseWriter, r *http.Request) (http.ResponseWriter, *http.Request)
+	Context *context.Context
+	AppHanlder *AppHandler
+}
+
+func (chain *MiddlewareChain) Add(f ...*func (w http.ResponseWriter, r *http.Request) (http.ResponseWriter, *http.Request)) *MiddlewareChain {
+	for i := 0; i < len(chain.MiddlewareHandlers); i++ {
+		chain.MiddlewareHandlers = append(chain.MiddlewareHandlers, f[i])
+	}
+	return chain
+}
+
+func (chain *MiddlewareChain) Handle() *http.Handler {
+	for i := 0; i < len(chain.MiddlewareHandlers); i++ {
+		
+	}
+	return nil
+}
+
 // AppHandler - basic hanlder for any requests/response before return any
 type AppHandler func(w http.ResponseWriter, r *http.Request) (interface{}, *error.AppError)
+
+func (chain *MiddlewareChain) AppHandler()
 
 // ServeHTTP - handle http req/res
 func (handler AppHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
